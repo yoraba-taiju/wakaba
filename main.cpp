9d250dea29f4ff3f7cab4aa72310066c7c40d485
Kaede Fujisaki <kaede@hexe.net>
2019-09-26 17:23:07 +0900

    手元のマシンが4.1まで対応してたので4.1にあげた

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ int main() {
 
   glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
