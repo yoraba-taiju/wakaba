88efd1a8a1c0c6d00b7fd45603fc6a5234ddc2a3
Kaede Fujisaki <kaede@hexe.net>
2019-10-30 21:37:55 +0900

    remove OpenGL suport

--- gl/vertex/ArrayBuffer.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by kaede on 2019/09/25.
-//
-
-#include "ArrayBuffer.hpp"
-
-namespace gl {
-
-}
--- gl/vertex/VertexArray.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by kaede on 2019/09/27.
-//
-
-#include "VertexArray.hpp"
-
-
-
